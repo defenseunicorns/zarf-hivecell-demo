7fd526640a593fd3fb52a187b7baab985651fcdf
Andrew Roth <roth.andy@gmail.com>
2021-11-09 15:57:12 -0800

    wip

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ fi
 ./zarf init --confirm --components k3s,container-registry,management --host localhost
 
 # Wait until the docker registry is ready
-timeout 300 bash -c 'while [[ "$(curl -sfSL --retry 15 --retry-connrefused --retry-delay 5 -o /dev/null -w "%{http_code}" "https://localhost/v2/")" != "200" ]]; do sleep 1; done' || false
+timeout 300 bash -c 'while [[ "$(curl -sfSL --retry 15 --retry-connrefused --retry-delay 5 -o /dev/null -w "%{http_code}" "https://localhost/v2/")" != "401" ]]; do sleep 1; done' || false
 
 # Deploy
 ./zarf package deploy --confirm zarf-package-appliance-demo-doom.tar.zst
