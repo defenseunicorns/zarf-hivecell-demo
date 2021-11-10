85da721c4a600490b38f3645e9e6d9aca510bcdc
Andrew Roth <roth.andy@gmail.com>
2021-11-09 16:08:25 -0800

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
 # We have to run as root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -13,7 +15,7 @@ fi
 ./zarf init --confirm --components k3s,container-registry,management --host localhost
 
 # Wait until the docker registry is ready
-timeout 300 bash -c 'while [[ "$(curl -sfSL --retry 15 --retry-connrefused --retry-delay 5 -o /dev/null -w "%{http_code}" "https://localhost/v2/")" != "401" ]]; do sleep 1; done' || false
+timeout --foreground 300 bash -c 'while [[ "$(curl -sfSL --retry 15 --retry-connrefused --retry-delay 5 -o /dev/null -w "%{http_code}" "https://localhost/v2/")" != "401" ]]; do sleep 1; done' || false
 
 # Deploy
 ./zarf package deploy --confirm zarf-package-appliance-demo-doom.tar.zst
